ec717b8be44046a3903ccaa8e403c9a67a0ce8d3
Juan Gonzales <anomalyarcadesticks@gmail.com>
2022-09-05 14:24:45 -0700

    Break

--- src/tests/githhub.test.js
+++ src/tests/githhub.test.js
@@ -4,38 +4,41 @@ describe("User Object", () => {
   //Multiple Describe Blocks
   //Verify if Plan = X then Disk _Usage = y
   //Contains expected Properties - Test for all Keys that come back
+  describe("#getUser", () => {
+    it("Should load user data: Name", async () => {
+      // given:
+      const user = "AnomalyAutomation";
+      const userName = "Juan Gonzales";
+      // when:
+      const res = await getUser(user);
+      //then:
+      expect(res.status.code).toEqual(200);
+      expect(res).toBeDefined();
+      expect(res.entity[0].name).toEqual(userName);
+    });
 
-  it("#getUser: Should load user data", async () => {
-    // given:
-    const user = "AnomalyAutomation";
-    const userName = "Juan Gonzales";
-    // when:
-    const res = await getUser(user);
-    //then:
-    expect(res.status.code).toEqual(200);
-    expect(res).toBeDefined();
-    expect(res.entity[0].name).toEqual(userName);
-  });
-
-  it("#getUser: ERROR: Undefined User Argument", () => {
-    return getUser().then((data) => {
-      expect(data).toBeDefined();
-      expect(data.status.code).toEqual(404);
-      expect(data.entity[0].message).toEqual("Not Found");
+    it("404 ERROR: Undefined User Argument", () => {
+      return getUser().then((data) => {
+        expect(data).toBeDefined();
+        expect(data.status.code).toEqual(404);
+        expect(data.entity[0].message).toEqual("Not Found");
+      });
     });
   });
 
-  it("#getUsersSince: Amount per Page", () => {
-    return getUsersSince(53061651, 10).then((data) => {
-      expect(data).toBeDefined();
-      expect(data.entity).toHaveLength(10);
+  describe("#getUsersSince", () => {
+    it("Amount should match Amount per Page Argument", () => {
+      return getUsersSince(53061651, 10).then((data) => {
+        expect(data).toBeDefined();
+        expect(data.entity).toHaveLength(10);
+      });
     });
-  });
 
-  it("#getUsersSince: Returns starting with User AFTER entered ID", () => {
-    return getUsersSince(53061650, 10).then((data) => {
-      expect(data).toBeDefined();
-      expect(data.entity[0].id).toEqual(53061651);
+    it("Should start with User Id after Entered Argument", () => {
+      return getUsersSince(53061650, 10).then((data) => {
+        expect(data).toBeDefined();
+        expect(data.entity[0].id).toEqual(53061651);
+      });
     });
   });
 });
